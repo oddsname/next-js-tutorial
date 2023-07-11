450e611995f08edff9ccb3576249e4f89e125176
oddsname <vadimich2012@mail.ru>
2023-07-11 15:01:17 +0300

    added some seo

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,4 +1,17 @@
 
+// static seo
+// export const metadata = {
+//     title: 'tetsddsds',
+// }
+
+//dynamic seo
+export function generateMetadata({params}) {
+
+    return {
+        title: 'dsasdsadas'
+    }
+}
+
 export default function () {
     return <>
         /posts
